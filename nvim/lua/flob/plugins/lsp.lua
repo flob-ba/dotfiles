f489670a57d32c02ec18a7d4bfd3ce553b046ab6
Florian Boisen <florianboisen@hotmail.de>
2024-10-28 21:00:10 +0100

    nvim: Update diagnostics in insert mode

--- nvim/lua/flob/plugins/lsp.lua
+++ nvim/lua/flob/plugins/lsp.lua
@@ -1,4 +1,17 @@
 return {
     {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-    {'neovim/nvim-lspconfig'},
+    {
+        'neovim/nvim-lspconfig',
+        opts = function ()
+            local ret = {
+                diagnostics = {
+                    update_in_insert = true,
+                }
+            }
+            return ret
+        end,
+        config = function (_,opts)
+            vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
+        end,
+    },
 }
